ca0a34364ca3faab551e351ba677c5db8dcc50b2
hlai16 <gloriajoanlai@gmail.com>
2021-11-04 23:51:55 -0400

    only able to write the .map() function

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
-
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
   render() {
     const dashboardClasses = classnames("dashboard");
+    const mapData = this.props.data.map(panel => 
+      <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} {...panel} />
+    )
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
